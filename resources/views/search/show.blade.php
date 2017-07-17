04d1fa129319cb5eac574fb82df58cf4469295e4
Han Khuu <h.khuu01@gmail.com>
2017-07-17 18:40:02 -0400

    added img icons

--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -2,29 +2,51 @@
 
 @section('content')
     <h1>{{ $selected->title }}</h1>
-{{--    {{dd($data)}}--}}
+    {{--{{dd($data)}}--}}
     {{--{{dd($selected)}}--}}
-    {{--{{dd($selected->analyzedInstructions[0]->steps[0]->number)}}--}}
-{{--    {{dd($selected->analyzedInstructions[0]->steps[0]->step)}}--}}
         <div id="recipeInfo">
             <img class="recipeImg" src="{{ $selected->image }}"/>
                 <div class="recipeText">
-                    <h2>Step-by-Step Instructions</h2>
-                    <p>Total Time: {{ $selected->readyInMinutes }} minutes</p>
+                    @if($selected->vegetarian === true)
+                        vegetarian
+                    @else
+                        not vegetarian
+                    @endif
 
-                    {{$selected->sourceUrl}}
+                    <p><strong>Total Time: {{ $selected->readyInMinutes }} minutes</strong>
+                    <br>
+                    Preparation: {{ $selected->preparationMinutes }} minutes
+                    <br>
+                    Cooking: {{ $selected->cookingMinutes }} minutes
+                    <br>
+                    Servings: {{$selected->servings}}</p>
+
+                    Source: <a href="{{$selected->sourceUrl}}" target="_blank">{{$selected->sourceName}}</a>
 
                     <hr>
 
+                    <h2>Ingredients</h2>
+                    @foreach($selected->extendedIngredients as $ingredient)
+                        {{$ingredient->originalString}} <br>
+                    @endforeach
+
+                    <hr>
+
+                    <h2>Step-by-Step Instructions</h2>
                     <ol>
                         @foreach($selected->analyzedInstructions[0]->steps as $step)
                                 {{$step->number}}. {{$step->step}} <br><br>
                         @endforeach
                     </ol>
 
+                    <hr>
+
+                    <h2>Nutritional Information</h2>
+                    @foreach($selected->nutrition->nutrients as $nutrition)
+                        {{$nutrition->amount}} {{$nutrition->unit}} {{$nutrition->title}}<br>
+                    @endforeach
 
                 </div>
         </div>
-    {{--@endforeach--}}
 
 @endsection
